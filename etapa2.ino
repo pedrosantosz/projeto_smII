9e225c65f32156d615f6851e5ca08abc502e9318
Pedro Santos <89491903+pedrosantosz@users.noreply.github.com>
2023-06-19 09:40:37 -0300

    Create etapa2.ino

---
+++ etapa2.ino
@@ -0,0 +1,45 @@
+#include <ShiftRegister74HC595.h>
+#define DATA 2
+#define LATCH 3
+#define CLOCK 4
+
+ShiftRegister74HC595 aLEDro(1, DATA, CLOCK, LATCH);
+
+int valorLido, armado = -1, pos = 128;
+unsigned long tinicial = millis();
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(A3, INPUT);
+  pinMode(DATA, OUTPUT);
+  pinMode(CLOCK, OUTPUT);
+  pinMode(LATCH, OUTPUT);
+
+  aLEDro.setAllLow();
+}
+
+void loop() {
+  valorLido = analogRead(A3);
+  if (valorLido >= 673 && valorLido <= 693) //503 ± 10
+    armado = 1;
+  else if (valorLido >= 843 && valorLido <= 853) //683 ± 10
+    armado = 0;
+
+  if (armado == 1) {
+    shiftOut(DATA, CLOCK, LSBFIRST, pos);
+    
+    if (millis() - tinicial >= 200) {
+      pos = pos / 2;
+      tinicial = millis();
+    }
+    
+    digitalWrite(LATCH, 1);  
+    digitalWrite(LATCH, 0);
+    
+    if (pos == 0)
+        pos = 128;
+  } else if (armado == 0) {
+    aLEDro.setAllLow();
+    pos = 0;
+  }
+}
