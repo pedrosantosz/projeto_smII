5880d964aad075885e424a623428cead9d047d74
Pedro Santos <89491903+pedrosantosz@users.noreply.github.com>
2023-06-26 18:39:48 -0300

    Update etapa1.ino

--- etapa1.ino
+++ etapa1.ino
@@ -1,5 +1,5 @@
 #include <ShiftDisplay.h>
-int valorLido, on = -1, atual=-1;
+int valorLido, on = -1, armado=-1;
 
 ShiftDisplay display(COMMON_CATHODE, 4);
  
@@ -16,12 +16,12 @@ void loop() {
   valorLido = analogRead(A3);
 
   if (on == 1) {
-    atual = 1;
+    armado = 1;
     display.set("on");
     digitalWrite(A1, 0);
     digitalWrite(A0, 1);    
   }else if (on == 0) {
-    atual = 0;
+    armado = 0;
     display.set("off");
     digitalWrite(A0, 0);
     digitalWrite(A1, 1);
@@ -31,14 +31,14 @@ void loop() {
 
   if (valorLido >= 673 && valorLido <= 693) {
     on = 1;
-    if (atual != 1) {
+    if (armado != 1) {
       tone(13, 12570);
       delay(350); 
       noTone(13);
     }
   }else if (valorLido >= 843 && valorLido <= 853) {
     on = 0;
-    if (atual != 0) {
+    if (armado != 0) {
       tone(13, 12570);
       delay(350);
       noTone(13);
