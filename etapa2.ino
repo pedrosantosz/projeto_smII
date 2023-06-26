4ba80d398581317ab4d51e21e60ef0f76edc7ab4
Pedro Santos <89491903+pedrosantosz@users.noreply.github.com>
2023-06-26 18:41:16 -0300

    Update etapa2.ino

--- etapa2.ino
+++ etapa2.ino
@@ -3,9 +3,9 @@
 #define LATCH 3
 #define CLOCK 4
 
-ShiftRegister74HC595 aLEDro(1, DATA, CLOCK, LATCH);
+ShiftRegister74HC595 leds(1, DATA, CLOCK, LATCH);
 
-int valorLido, valor = -1, pos = 128;
+int valorLido, on = -1, pos = 128;
 unsigned long tinicial = millis();
 
 void setup() {
@@ -15,17 +15,17 @@ void setup() {
   pinMode(CLOCK, OUTPUT);
   pinMode(LATCH, OUTPUT);
 
-  aLEDro.setAllLow();
+  leds.setAllLow();
 }
 
 void loop() {
   valorLido = analogRead(A3);
   if (valorLido >= 673 && valorLido <= 693)
-    valor = 1;
+    on = 1;
   else if (valorLido >= 843 && valorLido <= 853)
-    valor = 0;
+    on = 0;
 
-  if (valor == 1) {
+  if (on == 1) {
     shiftOut(DATA, CLOCK, LSBFIRST, pos);
     
     if (millis() - tinicial >= 200) {
@@ -39,7 +39,7 @@ void loop() {
     if (pos == 0)
         pos = 128;
   } else if (valor == 0) {
-    aLEDro.setAllLow();
+    leds.setAllLow();
     pos = 0;
   }
 }
