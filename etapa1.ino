d63824489139e775ebce92ceece7a01102fab249
Pedro Santos <89491903+pedrosantosz@users.noreply.github.com>
2023-06-26 19:01:10 -0300

    Update etapa1.ino

--- etapa1.ino
+++ etapa1.ino
@@ -1,13 +1,18 @@
 #include <ShiftDisplay.h>
+
+#define RED
+#define GREEN
+#define BUZZER
+
 int valorLido, on = -1, armado=-1;
 
 ShiftDisplay display(COMMON_CATHODE, 4);
  
 void setup() {
-  pinMode(A0, OUTPUT);
-  pinMode(A1, OUTPUT);
+  pinMode(RED, OUTPUT);
+  pinMode(GREEN, OUTPUT);
   pinMode(A3, INPUT);
-  pinMode(13, INPUT);
+  pinMode(BUZZER, INPUT);
 
   digitalWrite(A1, 1);
 }
@@ -18,13 +23,13 @@ void loop() {
   if (on == 1) {
     armado = 1;
     display.set("on");
-    digitalWrite(A1, 0);
-    digitalWrite(A0, 1);    
+    digitalWrite(RED, 1);  
+    digitalWrite(GREEN, 0);
   }else if (on == 0) {
     armado = 0;
     display.set("off");
-    digitalWrite(A0, 0);
-    digitalWrite(A1, 1);
+    digitalWrite(RED, 0);
+    digitalWrite(GREEN, 1);
   } else if (on == -1) {
     display.set("off");
   }
@@ -32,18 +37,17 @@ void loop() {
   if (valorLido >= 673 && valorLido <= 693) {
     on = 1;
     if (armado != 1) {
-      tone(13, 12570);
+      tone(BUZZER, 12570);
       delay(350); 
-      noTone(13);
+      noTone(BUZZER);
     }
   }else if (valorLido >= 843 && valorLido <= 853) {
     on = 0;
     if (armado != 0) {
-      tone(13, 12570);
+      tone(BUZZER, 12570);
       delay(350);
-      noTone(13);
+      noTone(BUZZER);
     }
   }
-
   display.show();
 }
